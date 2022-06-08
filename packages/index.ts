0b306ebb146ba04d0d3175d9c0a4fa975e739ade
chengbotao <chengbotao5221@163.com>
2022-06-08 10:23:23 +0800

    init: 工程初始化

---
+++ packages/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-07 17:45:04
+ */
+
+export function test() {
+  const a = 1;
+  return a;
+}
---
+++ rollup.config.ts
@@ -0,0 +1,59 @@
+/*
+ * @Author: Chengbotao
+ * @Date: 2022-06-07 10:34:13
+ */
+import { resolve } from 'path';
+import pkg from './package.json';
+
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import alias from '@rollup/plugin-alias';
+import eslint from '@rollup/plugin-eslint';
+// import { babel } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+
+const { version } = pkg;
+const outputConf = {
+  banner: `/* Billows version ${version} */`,
+  footer: `/* Follow me on GitHub! @chengbotao */`,
+};
+
+export default {
+  input: 'packages/index.ts',
+  output: [
+    {
+      file: resolve(__dirname, 'dist', 'billows.esm.js'),
+      format: 'esm',
+      ...outputConf,
+    },
+    {
+      file: resolve(__dirname, 'dist', 'billows.js'),
+      format: 'umd',
+      name: 'billows',
+      ...outputConf,
+    },
+  ],
+  plugins: [
+    alias({
+      entries: {
+        packages: resolve(__dirname, 'packages'),
+      },
+    }),
+    nodeResolve({
+      extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'],
+    }),
+    commonjs(),
+    eslint({
+      // fix: true,
+      include: ['packages/**/*.ts'],
+      exclude: ['node_modules/**'],
+    }),
+    // babel({
+    //   babelHelpers: 'bundled',
+    //   extensions: ['ts', 'tsx', '.js', '.jsx', '.es6', '.es', '.mjs'],
+    // }),
+    typescript(),
+    json(),
+  ],
+};
